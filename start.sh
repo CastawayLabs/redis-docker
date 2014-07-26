d66ce4ef064bdb41e12e08b934df2dcb3acf3f43
Matej Kramny <git@matej.me>
2014-07-26 19:13:22 +0100

    Add passworded redis ENV variable

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$REDIS_PASSWORD" ]; then
+  echo 'requirepass $REDIS_PASSWORD' >> /etc/redis.conf
+fi
+
+/usr/local/bin/redis-server /etc/redis.conf
+if [ $? -ne 0 ]; then
+  echo App Quit
+  exit 1
+fi
+
+exit 0
